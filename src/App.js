58a82b5ac601ec55078c90509f7c54b2b281d9c7
beliang <b3nliang@gmail.com>
2023-08-21 02:02:03 -0700

    Added firebase auth to merge with main after reset

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { RouterProvider } from 'react-router';
 import './App.css';
 import AudioManager from './components/AudioManager';
 import React from 'react';
+import { router } from "../src/lib/routes"
 
 function App() {
   return (
     <div className="App">
-      <AudioManager />
+      <RouterProvider router ={router}/>
     </div>
   );
 }
---
+++ src/components/layout/index.js
@@ -0,0 +1,22 @@
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
+import React, { useEffect } from "react"
+import { useAuth } from "../../hooks/auth"
+
+export default function Layout() {
+    const location = useLocation();
+    const {user, isLoading} = useAuth();
+    const navigate = useNavigate();
+    useEffect(() => {
+        console.log(location.pathname.startsWith("/protected"));
+        console.log(!user);
+        if(location.pathname.startsWith("/protected") && !user) {
+            navigate("/login");
+        }
+    }, [location.pathname, user]);
+
+    if(isLoading) return "Currently Loading...";
+
+    return (
+        <Outlet/>
+    )
+}
